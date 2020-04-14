67c911bd7407c6a99c5225b471329dc365a9304a
Adam Leskis <leskis@gmail.com>
2020-04-14 20:04:23 +0100

    re-style it like skeletor

--- src/components/PRList.js
+++ src/components/PRList.js
@@ -2,53 +2,20 @@ import React, { useEffect, useState} from 'react';
 import axios from 'axios';
 
 import PRUrlItem from './PRUrlItem';
-import DiffModal from './DiffModal';
 
 const REQUEST_URL = 'https://api.github.com/search/issues?q=label:pr-review-request+is:pr+state:open&sort=created&order=asc'
 
 const PRList = () => {
 
-    const showDiffModal = ({ diffUrl }) => {
-        // setDiffUrl(diffUrl);
-        setShowModal(true);
-    }
-
-    const handleClick = (diffUrl) => {
-        setDiffUrl(diffUrl)
-        console.log(`clicked on ${diffUrl}`);
-    }
-
-    const hideDiffModal = () => setShowModal(false);
-
     const [PRs, setPRs] = useState([]);
-    const [showModal, setShowModal] = useState(false);
-    const [diff, setDiff] = useState();
-    const [diffUrl, setDiffUrl] = useState(null)
-
-    // grab the diff and set it in state
-    const fetchDiff = async (diffUrl) => {
-        console.log(diffUrl)
-      const result = await axios(
-        diffUrl
-      );
-      console.log(result.data);
-      setDiff(result.data);
-    };
 
     // grab this on page load
     const fetchData = async () => {
       const result = await axios(
         REQUEST_URL
       );
-      console.log(result.data.items);
       setPRs(result.data.items);
     };
-
-    // only grab this when the diffUrl changes
-    // useEffect(() => {
-        // fetchDiff(diffUrl)
-        // setShowModal(true)
-    // }, [diffUrl])
 
     // get the PRs with the right label on page load
     useEffect(() => {
@@ -57,10 +24,9 @@ const PRList = () => {
 
     return (
         <ul className="pr-list">
-          {showModal && <DiffModal onClickModal={hideDiffModal} diff={diff} />}
-          {!showModal && PRs.length === 0 && <div className="no-data">no data yet...</div>}
-          {!showModal && PRs.map(item => (
-            <PRUrlItem url={item.html_url} diffUrl={item.pull_request.diff_url} onClick={handleClick} />
+          {PRs.length === 0 && <div className="no-data">no data yet...</div>}
+          {PRs.map(item => (
+            <PRUrlItem key={item.html_url} url={item.html_url} diffUrl={item.pull_request.diff_url} />
           ))}
         </ul>
     );
--- src/components/PRUrlItem.js
+++ src/components/PRUrlItem.js
@@ -2,11 +2,10 @@ import React from 'react';
 
 const PRUrlItem = ({
     diffUrl,
-    onClick,
     url,
 }) => {
     return (
-        <li key={url} onClick={() => onClick(diffUrl)} className="pr-url">
+        <li className="pr-url">
             <a href={url}>{url}</a>
         </li>
     )
